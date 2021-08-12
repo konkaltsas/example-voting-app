592af0100cfc5bd021b26c84d791114776ac9596
Konstantinos Kaltsas <konkaltsas@icloud.com>
2021-08-12 14:10:47 +0300

    Update app.py

--- vote/app.py
+++ vote/app.py
@@ -19,8 +19,8 @@ app.logger.setLevel(logging.INFO)
 
 def get_redis():
     #if not hasattr(g, 'redis'):
-        g.redis = Redis(host="redis", db=0, password=redis_password ,socket_timeout=5)
-    return g.redis
+        #g.redis = Redis(host="redis", db=0, password=redis_password ,socket_timeout=5)
+    return Redis(host="redis", db=0, password=redis_password ,socket_timeout=5)
 
 @app.route("/", methods=['POST','GET'])
 def hello():
